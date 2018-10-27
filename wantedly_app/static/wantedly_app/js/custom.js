20b1e8851c58ddbe7526fdc1bda02c74beb29e9a
hodanov <1031hoda@gmail.com>
2018-10-27 17:58:21 +0000

    Implement featured-slider

--- wantedly_app/static/wantedly_app/js/custom.js
+++ wantedly_app/static/wantedly_app/js/custom.js
@@ -10,120 +10,90 @@ $(document).ready(function() {
   .then(featuredSlider)
 })
 
-// $(document).ready(function(){
-
 // ---Fade-out effect---
 function fadeOutEffect() {
   $(window).scroll(function() {
-    var scrolled = $(window).scrollTop();
-    var backgroundContainer = '.background2 > .responsive-img';
+    var scrolled = $(window).scrollTop()
+    var backgroundContainer = '.background2 > .responsive-img'
     $('.hero').each(function(index, element) {
       // var initY = $(this).offset().top
-      var initY = 0;
-      var height = $(this).height();
-      var width = $(this).width();
-      var endY  = initY + $(this).height();
+      var initY = 0
+      var height = $(this).height()
+      var width = $(this).width()
+      var endY  = initY + $(this).height()
 
       // Check if the element is in the viewport.
-      var visible = isInViewport(this);
+      var visible = isInViewport(this)
       if(visible) {
-        var diff = scrolled - initY;
-        var ratio = (1 - (diff / height) * 1.5);
-        $(backgroundContainer).css('opacity', ratio);
+        var diff = scrolled - initY
+        var ratio = (1 - (diff / height) * 1.5)
+        $(backgroundContainer).css('opacity', ratio)
       }
-    });
-  });
+    })
+  })
   function isInViewport(node) {
-    var rect = node.getBoundingClientRect();
+    var rect = node.getBoundingClientRect()
     return (
       (rect.height > 0 || rect.width > 0) &&
       rect.bottom >= 0 &&
       rect.right >= 0 &&
       rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
       rect.left <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+    )
   }
 }
 
 // ---Slider---
 function featuredSlider() {
   if ($('.slide-window')) {
+    var slideWrapper = $('.slide-wrapper')
     var slideNum = $('.slide').length - 1
     var maxSlideNum = $('.slide').length - 1
     var slideWidth = $('.slide-window').outerWidth()
     var slideSetWidth = slideWidth * slideNum
-    var controlSlideBtnLeft = '.slide-control-left'
-    var controlSlideBtnRight = '.slide-control-right'
-    var controlSlideBtnList = '.slide-control-btn ul li a'
+    var prevBtn = '.slide-control-left'
+    var nextBtn = '.slide-control-right'
+    var direction
+    var clickFlg = true
 
-    addSlideCtlBtn()
-
-    var slideCurrent = 0
-    var slideActiveBtn = 0
     var rotationInterval = setInterval(function(){
-      slideCurrent++
-      if (slideCurrent > maxSlideNum) {
-        slideCurrent = 0
-      }
-      activation()
-      sliding()
+      moveSlide(direction = 'right')
     }, 10000)
 
-    $(controlSlideBtnLeft).on('click', function(){
-      resetInterval()
-      slideCurrent--
-      if (slideCurrent < 0) {
-        slideCurrent = maxSlideNum
-      }
-      activation()
-      rotateArrow('left')
-      sliding()
-    })
-    $(controlSlideBtnRight).on('click', function(){
-      resetInterval()
-      slideCurrent++
-      if (slideCurrent > maxSlideNum) {
-        slideCurrent = 0
-      }
-      activation()
-      rotateArrow('right')
-      sliding()
-    })
-    $(controlSlideBtnList).on('click', function(){
-      resetInterval()
-      slideCurrent = $(controlSlideBtnList).index(this)
-      activation()
-      sliding()
+    $(window).bind("focus",function() {
+      rotationInterval = setInterval(function(){
+        moveSlide(direction = 'right')
+      }, 10000)
+    }).bind("blur", function() {
+      clearInterval(rotationInterval)
     })
 
-    function addSlideCtlBtn() {
-      var slideCtlBtn = '<div class="slide-control-btn"><ul>'
-      for (var i = 0; i <= maxSlideNum; i++) {
-        if (i==0) {
-          slideCtlBtn += '<li><a class="active"></a></li>'
-        }
-        else {
-          slideCtlBtn += '<li><a class=""></a></li>'
-        }
+    $(prevBtn).on('click', function(){
+      if (clickFlg) {
+        clickFlg = false
+        startProcess(direction = 'left')
       }
-      slideCtlBtn += '</ul></div>'
-      $('.slide-controller').append(slideCtlBtn)
+    })
+    $(nextBtn).on('click', function(){
+      if (clickFlg) {
+        clickFlg = false
+        startProcess(direction = 'right')
+      }
+    })
+
+    function startProcess(direction) {
+      resetInterval()
+      rotateArrow(direction)
+      moveSlide(direction)
+    }
+    function moveSlideTimer() {
+      moveSlide(direction = 'right')
     }
     function resetInterval() {
       clearInterval(rotationInterval)
       rotationInterval = setInterval(function(){
-        slideCurrent++
-        if (slideCurrent > maxSlideNum) {
-          slideCurrent = 0
-        }
-        activation()
-        sliding()
+        moveSlide(direction = 'right')
       }, 10000)
-    }
-    function activation() {
-      slideActiveBtn = slideCurrent + 1
-      $( controlSlideBtnList + '.active' ).removeClass()
-      $('.slide-control-btn ul li:nth-child(' + slideActiveBtn + ') a').addClass('active')
     }
     function rotateArrow(direction) {
       var slideCtlLine1 = '.slide-control-' + direction + ' .slide-control-line:nth-child(1)'
@@ -135,8 +105,29 @@ function featuredSlider() {
       $(slideCtlLine1).addClass('rotate-' + direction + '-top')
       $(slideCtlLine2).addClass('rotate-' + direction + '-down')
     }
-    function sliding() {
-      $('.slide-wrapper').css('left', slideCurrent * -slideWidth)
+    function moveSlide(direction) {
+      slideWrapper.addClass('transition')
+      if (direction === 'left') {
+        slideWrapper.css('transform', 'translateX(' + -slideWidth + 'px)')
+        $('.slide:last').clone().prependTo(slideWrapper)
+        slideWrapper.css('left', 0).one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+        function(e) {
+          $('.slide:last').remove()
+          slideWrapper.removeClass('transition')
+          slideWrapper.css({'left': - slideWidth, 'transform': ''})
+          clickFlg = true
+        })
+      }
+      else if (direction === 'right') {
+        $('.slide:first').clone().appendTo(slideWrapper)
+        slideWrapper.css('left', 2 * -slideWidth).one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+        function(e) {
+          $('.slide:first').remove()
+          slideWrapper.removeClass('transition')
+          slideWrapper.css('left', - slideWidth)
+          clickFlg = true
+        })
+      }
     }
   }
 }
